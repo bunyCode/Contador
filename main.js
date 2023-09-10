64050db9632cb4d268f3d359651bbcbc5b33f141
Daniel fernando reyes v <dfreyes1236@gmail.com>
2023-09-10 14:20:25 -0500

    He subido el codigo del contador

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const contar = document.getElementById('contar');
+const restar = document.getElementById('restar');
+const sumar = document.getElementById('suma');
+const restableser = document.getElementById('restablecer');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let sumarest = 0;
 
-setupCounter(document.querySelector('#counter'))
+sumar.addEventListener('click', () => {
+    sumarest++
+    contar.innerText = sumarest
+})
+
+restar.addEventListener('click', () => {
+    if (sumarest <= 0) return
+    sumarest--;
+    contar.innerText = sumarest
+})
+
+restableser.addEventListener('click', () => {
+    sumarest = 0;
+    contar.innerText = 0;
+})
